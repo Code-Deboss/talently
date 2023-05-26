d4d905582217640ce3f00d837622dca5483bdbdc
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-05-26 11:37:20 +0100

    changes made

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path="/feed" element={ <Feed /> } />
-          <Route path="/login" element={ <Signup /> } />
+          <Route path="/signup" element={ <Signup /> } />
         </Routes>
       </Router>
     </>
--- src/pages/login_signup_layout/Signup.js
+++ src/pages/login_signup_layout/Signup.js
@@ -3,11 +3,12 @@ import './signup.css'
 import { Link } from 'react-router-dom'
 import Img from './logo.png'
 import Button from './Button/Button'
+import ImgII from "./assets/premium_photo-1683639549573-bcc2db33dc15.avif"
 
 const Signup = () => {
   return (
     <>
-      <header className='fixed top-0 left-0 w-full z-[100]  flex items-center justify-around -p-2 px-9 bg-blur-sm shadow-md flex-wrap'>
+      <header className='fixed top-0 left-0 w-full z-[100]  flex items-center justify-around -p-2 px-9 bg-blur-sm  flex-wrap'>
        <div className=''>
         <Link to={'/'} className="-m-1.5 p-1.5">
               <span className="sr-only">Talently Logo</span>
@@ -19,7 +20,7 @@ const Signup = () => {
             </Link>
        </div>
        <div className=''>
-         <Link to={'/signup'} className='no-underline'>
+         <Link to={'/login'} className='no-underline'>
               <Button
                 buttonClass={'xl:py-3 px-3 py-2 xl:px-5 rounded-md bg-transparent border border-indigo-500 text-indigo-500 text-base font-normal'}
                 buttonText={'Login'}
@@ -27,13 +28,43 @@ const Signup = () => {
             </Link>
        </div>
       </header>
-      <main className='h-screen w-full bg-blue-200 flex items-center flex-wrap justify-items-center flex-row'>
+      <main className='h-screen w-full flex items-center flex-wrap justify-items-center flex-row bg-blue-300'>
         
-        <div className='h-screen w-5/12 bg-red-200'>
-          
+        <div className='h-screen w-[45%] flex items-center justify-items-center overflow-hidden img-container'>
+          <img src={ImgII} className='w-full h-full object-cover object-center' alt=''/>
         </div>
-        <div>
-          
+        <div className='flex items-center justify-items-center h-screen w-[55%] form ' >
+          <form className='h-[740px] w-[580px] bg-gray-100 ml-[70px] mt-[5px] flex items-start justify-items-between flex-col '>
+            <div>
+              <h1 className='text-3xl m-0 '>Get started</h1>
+              <p>Create your Account</p>
+            </div>
+            <div>
+              <input />
+              <input />
+              <input />
+              <input />
+            </div>
+            <div>
+              <p>Select User Type</p>
+              <div>
+                <input type='checkbox' />
+                <h3>Talents</h3>
+                <p>I want to showcase my talents here</p>
+              </div>
+              <div>
+                <input type='checkbox' />
+                <h3>Scouts</h3>
+                <p>I want to see those who are showcasing talents</p>
+              </div>
+              <div>
+                <input type='checkbox' />
+                <h3>None</h3>
+                <p>I want to be able to vote for talents.</p>
+              </div>
+            </div>
+            <p>By clicking Create my account, you agree to Know Talently <span>User Agreement, Privacy Policy</span>, and <span>Cookie Policy</span></p>
+          </form>
         </div>
 
       </main>
